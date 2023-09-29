7d0baf9a36668145569b77763f4c35a3628e8b88
prashantsingh-nickelfox <prashant.singh@nickelfox.com>
2023-09-29 13:10:44 +0530

    fix(): remove useEffect

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { PRODUCTS } from "../products";
 import { getProduct } from "../components/Analytics";
 
